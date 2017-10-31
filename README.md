4c8094991431f57698209d5bbc515adad46f10ad
niki <24849691+niki@users.noreply.github.com>
2017-10-31 13:23:07 +0900

    Fix VC files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+
 - liblua.sln  
-  Generate a static library, liblua53.lib.
+  Generate a static library, liblua53.lib, liblua53_64.lib.
+
+- lua-rebuild.bat
+  Generate a dynamic library, lua53.lib, lua53.dll.
+
+- lua-rebuild64.bat
+  Generate a dynamic library, lua53_64.lib, lua53_64.dll.
+
+
--- lua-rebuild.bat
+++ lua-rebuild.bat
@@ -1,8 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-copy Lua.Makefile.nmake .\lua\src\Makefile.nmake
+copy Lua.Makefile.nmake .\src\Makefile.nmake
 
-cd .\lua\src
+pushd .\src
 
 nmake /f Makefile.nmake clean
 
@@ -10,7 +10,9 @@ nmake /f Makefile.nmake dll x86=1
 copy lua53.lib ..
 copy lua53.dll ..
 
-nmake /f Makefile.nmake lib x86=1
-copy liblua53.lib ..
+::nmake /f Makefile.nmake lib x86=1
+::copy liblua53.lib ..
+
+popd
 
 pause
